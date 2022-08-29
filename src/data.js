2ab37f5e4ecbd72d54ecfc48797d91b88366bd28
Nevena Djokic <djnevena99@gmail.com>
2022-08-29 20:59:14 +0200

    izmena css-a, dodavanje novih usliga na pocetnu stranicu

--- src/data.js
+++ src/data.js
@@ -4,15 +4,15 @@ export const postInfo = [
         headerTitle: 'FARBANJE KOSE',
         headerSubtitle: 'Dostupne sve boje',
         headerText: 'Cene od 1000 dinara.',
-        title: 'Frizerski salon',
+
         img: '.././images/farbanje.jpg'
     },
     {
         id: 2,
-        headerTitle: 'ŠIŠANJE',
+        headerTitle: 'ŽENSKO ŠIŠANJE',
         headerSubtitle: 'Sve dužine',
         headerText: 'Cene od 500 dinara.',
-        title: 'Frizerski salon',
+
         img: '.././images/sisanje.jpg'
     },
     {
@@ -20,7 +20,7 @@ export const postInfo = [
         headerTitle: 'UVIJANJE KOSE',
         headerSubtitle: 'Sve vrste lokni',
         headerText: 'Cene od 800 dinara.',
-        title: 'Frizerski salon',
+
         img: '.././images/uvijanje.jpg'
     },
 
@@ -29,7 +29,7 @@ export const postInfo = [
         headerTitle: 'ISPRAVLJANJE KOSE',
         headerSubtitle: 'Feniranje/pegla',
         headerText: 'Cene od 500 dinara.',
-        title: 'Frizerski salon',
+        
         img: '.././images/peglanje.jpg'
     },
 
@@ -38,7 +38,7 @@ export const postInfo = [
         headerTitle: 'NADOGRADNJA KOSE',
         headerSubtitle: 'Metod nadogradnje: klipse',
         headerText: 'Cene od 5050 dinara.',
-        title: 'Frizerski salon', 
+
         img: '.././images/nadogradnja.jpg'
     },
 
@@ -47,8 +47,34 @@ export const postInfo = [
         headerTitle: 'PRANJE KOSE',
         headerSubtitle: 'Pranje + feniranje',
         headerText: 'Cene od 600 dinara.',
-        title: 'Frizerski salon',
+
         img: '.././images/pranje.png'
     },
-    //dodati cenovnik?
+
+    {
+        id: 6,
+        headerTitle: 'SVEČANE FRIZURE',
+        headerSubtitle: 'Za sve vrste svečanosti',
+        headerText: 'Cene od 800 dinara.',
+
+        img: '.././images/svecana.jpg'
+    },
+
+    {
+        id: 6,
+        headerTitle: 'MUŠKO ŠIŠANJE',
+        headerSubtitle: 'Stilizovanje po želji',
+        headerText: 'Cene od 400 dinara.',
+
+        img: '.././images/musko.jpg'
+    },
+
+    {
+        id: 6,
+        headerTitle: 'BALAYAGE',
+        headerSubtitle: 'Kolor + preliv',
+        headerText: 'Cene od 4000 dinara.',
+
+        img: '.././images/balejaz.jpg'
+    },
 ];
